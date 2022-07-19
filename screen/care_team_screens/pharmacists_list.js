bb9b36bbfda97c72b24eb64e3458173e6537ab6d
Ian Brown <ibrown@bear-code.com>
2022-07-19 08:31:05 -0400

    Added pharmacist list page.

--- screen/care_team_screens/pharmacists_list.js
+++ screen/care_team_screens/pharmacists_list.js
@@ -1,15 +1,72 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ListView } from '../components'
+import { patientPharmacistApi } from "../../api"
+import { valueHelper, alertHelper, patientHelper, currentUserHelper, patientPharmacistHelper } from '../../helpers'
 import { styles } from '../../assets/styles'
 
-function PharmacistsList(_props) {
+function Pharmacist(props) {
+  const { pharmacist } = props
+
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={styles.mainBody}>
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Not implemented yet</Text>
-    </ScrollView>
+    <TouchableOpacity
+      activeOpacity={0.5}
+      style={styles.listButton}>
+      <View style={{ flexDirection: 'column', alignItems: 'center', width: '95%' }}>
+        <View style={{ flexDirection: "row" }}>
+          <Image style={{ ...styles.icon, width: 23, height: 20 }} source={require('../../assets/pharmacist.jpg')} />
+          <Text style={inlineStyles.text}>{patientPharmacistHelper.name(pharmacist)} ({patientPharmacistHelper.roleLabel(pharmacist)})</Text>
+        </View>
+        <Text style={inlineStyles.text}>NPI: {patientPharmacistHelper.pharmacistNpi(pharmacist)}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
+function PharmacistsList(props) {
+  const { navigation } = props
+  const { currentPatient, userCredentials } = currentUserHelper.getCurrentProps(props)
+
+  return (
+    <View style={styles.mainBody}>
+      <ListView
+        label='Pharmacist'
+        navigation={navigation}
+        onLoadPage={loadPage}
+        onPresentItem={presentItem}
+        pageSize={20}
+        pluralLabel='Pharmacists'
+      />
+    </View>
+  )
+
+  function loadPage(number, size, onSuccess) {
+    patientPharmacistApi.listForPatient(
+      userCredentials,
+      patientHelper.id(currentPatient),
+      { for_active: true, page: { number, size, total: 0 }, sort: 'last_name,first_name' },
+      onSuccess,
+      alertHelper.handleError
+    )
+  }
+
+  function presentItem(pharmacist, pharmacistIdx, editPharmacist) {
+    return (
+      <Pharmacist
+        key={`pharmacist-${patientPharmacistHelper.id(pharmacist)}-${pharmacistIdx}`}
+        pharmacist={pharmacist}
+        //onDelete={deletePharmacist}
+        onEdit={editPharmacist}
+        {...props}
+      />
+    )
+  }
+}
+
 export { PharmacistsList }
+
+const inlineStyles = StyleSheet.create(
+  {
+    text: { color: "#112B37", fontSize: 18, fontWeight: "500", marginLeft: 5 }
+  }
+)
